c179855c708da90b8e18f0daa64916a8b749d0e0
Sander <bitbucket@ugoku.nl>
2018-01-30 10:39:19 +0100

    Version bump

--- instagram-feed.php
+++ instagram-feed.php
@@ -23,7 +23,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define('SBIVER', '1.7.0');
+define('SBIVER', '1.7.1');
 
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
